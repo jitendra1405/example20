768e6676ff1201df1c5ceb157b9fd9d479fd2e91
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-13 15:10:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -32,16 +32,7 @@
 <?php
 class DB_Connect
 {
-    public function connect()
-    {
-      $host = "ec2-50-19-124-157.compute-1.amazonaws.com";
-	  $user = "nfltllotkrgbnc";
-	  $password = "6501404a979c7d7bb1da09c71cd54a6b83fb4986354072611d25bf3c0f0287e0";
-	  $dbname = "d58o173hbaukt7";
-	  $port = "5432";
-
-      $db = pg_connect( " $url $host $port $dbname $credentials"  );
-      if(!$db){
+       if(!$pdo){
          echo "Error : Unable to open database\n";
       } else {
          echo "Opened database successfully\n";
